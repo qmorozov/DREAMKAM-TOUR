69584f062731815ab8ff07c88989e64046d8397a
qamorozov <andr.morozov96@gmail.com>
2021-06-15 16:22:33 +0300

    code improvement

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,17 @@
 const slider = document.querySelector('.slider__container');
 
-let swiper = new Swiper(slider, {
+let swiper = new Swiper('.slider__container', {
     spaceBetween: 35,
-    slidesPerView: 2.5,
+    // slidesPerView: 2.5,
+    slidesPerView: 'auto',
     grabCursor: true,
     loop: true,
-    mousewheel: {
-        invert: true,
-    },
+    // mousewheel: {
+    //     invert: true,
+    // },
     slideClass: 'slider__item',
     wrapperClass: 'slider__wrapper',
-    slideActiveClass: 'slider__item-active',
+    slideActiveClass: 'slider__item_active',
     navigation: {
         nextEl: '.slider__button-next',
         prevEl: '.slider__button-prev',

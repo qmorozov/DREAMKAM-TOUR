21270a5878ccfa46128bcd01e5e7784772cb99e2
qamorozov <andr.morozov96@gmail.com>
2021-06-23 21:53:19 +0300

    header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -32,25 +32,82 @@ let aboutSwiper = new Swiper('.about-slider', {
 
 //////////////////////////////
 
-const minus = document.querySelector('.btn-minus'),
-    plus = document.querySelector('.btn-plus'),
-    requestInput = document.querySelector('.request-input');
+// const minus = document.querySelector('.btn-minus'),
+//     plus = document.querySelector('.btn-plus'),
+//     requestInput = document.querySelector('.request-input');
 
-minus.addEventListener('click', () => {
-    let total = requestInput.value;
-    total--;
-    requestInput.value = total;
-    if (requestInput.value < 1) {
-        requestInput.value = 1;
-    }
-});
+// minus.addEventListener('click', () => {
+//     let total = requestInput.value;
+//     total--;
+//     requestInput.value = total;
+//     if (requestInput.value < 1) {
+//         requestInput.value = 1;
+//     }
+// });
 
-plus.addEventListener('click', () => {
-    let total = requestInput.value;
-    total++;
-    requestInput.value = total;
-    if (requestInput.value > 20) {
-        requestInput.value = 20;
-    }
-});
+// plus.addEventListener('click', () => {
+//     let total = requestInput.value;
+//     total++;
+//     requestInput.value = total;
+//     if (requestInput.value > 20) {
+//         requestInput.value = 20;
+//     }
+// });
 ////////////
+const navigation = document.querySelector('.header');
+const btn = document.querySelector('.burger');
+btn.addEventListener('click', function(event) {
+    btn.classList.toggle('burger_active');
+    navigation.classList.toggle('header_active');
+    if (navigation.classList.contains('header_active')) {
+        hideScroll();
+    } else {
+        showScroll();
+    }
+});
+
+const hideScroll = () => {
+    const scrollWidth = `${getScrollbarWidth()}px`;
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
+    navigation.style.paddingRight = scrollWidth;
+};
+
+const showScroll = () => {
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
+    navigation.style.paddingRight = '';
+};
+
+const getScrollbarWidth = () => {
+    const outer = document.createElement('div');
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
+    document.body.appendChild(outer);
+    const ScrollBarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
+    return ScrollBarWidth;
+};
+
+////////////////
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+        btn.classList.remove('burger_active');
+        navigation.classList.remove('header_active');
+        showScroll();
+    });
+};
+////////////////////

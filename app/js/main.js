965bee8c0a79554907a728b4b716ce7795103760
qamorozov <andr.morozov96@gmail.com>
2021-06-27 09:12:48 +0300

    bug fixing

--- app/js/main.js
+++ app/js/main.js
@@ -114,39 +114,39 @@ const navigation = document.querySelector('.header'),
 btn.addEventListener('click', function(event) {
     btn.classList.toggle('burger_active');
     navigation.classList.toggle('header_active');
-    // if (navigation.classList.contains('header_active')) {
-    //     hideScroll();
-    // } else {
-    //     showScroll();
-    // }
+    if (navigation.classList.contains('header_active')) {
+        hideScroll();
+    } else {
+        showScroll();
+    }
 });
 
-// const hideScroll = () => {
-//     const scrollWidth = `${getScrollbarWidth()}px`;
-//     document.body.style.paddingRight = scrollWidth;
-//     document.body.style.overflow = 'hidden';
-//     navigation.style.paddingRight = scrollWidth;
-// };
+const hideScroll = () => {
+    const scrollWidth = `${getScrollbarWidth()}px`;
+    document.body.style.paddingRight = scrollWidth;
+    document.body.style.overflow = 'hidden';
+    navigation.style.paddingRight = scrollWidth;
+};
 
-// const showScroll = () => {
-//     document.body.style.paddingRight = '';
-//     document.body.style.overflow = 'visible';
-//     navigation.style.paddingRight = '';
-// };
+const showScroll = () => {
+    document.body.style.paddingRight = '';
+    document.body.style.overflow = 'visible';
+    navigation.style.paddingRight = '';
+};
 
-// const getScrollbarWidth = () => {
-//     const outer = document.createElement('div');
-//     outer.style.position = 'absolute';
-//     outer.style.top = '-9999px';
-//     outer.style.width = '50px';
-//     outer.style.height = '50px';
-//     outer.style.overflow = 'scroll';
-//     outer.style.visibility = 'hidden';
-//     document.body.appendChild(outer);
-//     const ScrollBarWidth = outer.offsetWidth - outer.clientWidth;
-//     document.body.removeChild(outer);
-//     return ScrollBarWidth;
-// };
+const getScrollbarWidth = () => {
+    const outer = document.createElement('div');
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '50px';
+    outer.style.height = '50px';
+    outer.style.overflow = 'scroll';
+    outer.style.visibility = 'hidden';
+    document.body.appendChild(outer);
+    const ScrollBarWidth = outer.offsetWidth - outer.clientWidth;
+    document.body.removeChild(outer);
+    return ScrollBarWidth;
+};
 
 ////////////////
 
